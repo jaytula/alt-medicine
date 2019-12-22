410e960ab7dd36f88a7fdb14791a6788211adecd
Jay Tulathimutte <jaytula@gmail.com>
2019-12-23 03:34:52 +0700

    Updates

---
+++ precache-manifest.5fb5a5e8a1ddbf387b3fde38bccbbbad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49260d1f1bad291417ff19cc2045c5d",
+    "url": "/alt-medicine/index.html"
+  },
+  {
+    "revision": "a0e5321ff7d47c66efb7",
+    "url": "/alt-medicine/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6979e3a9b539d093f6ab",
+    "url": "/alt-medicine/static/js/2.5994dc75.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/alt-medicine/static/js/2.5994dc75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0e5321ff7d47c66efb7",
+    "url": "/alt-medicine/static/js/main.33fee09d.chunk.js"
+  },
+  {
+    "revision": "e246ca9b950592bdda55",
+    "url": "/alt-medicine/static/js/runtime-main.2844b414.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alt-medicine/static/media/logo.5d5d9eef.svg"
+  }
+]);
