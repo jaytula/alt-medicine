1d4d07a1a56ecf419a0a69b1e85939809ae8d2e9
Jay Tulathimutte <jaytula@gmail.com>
2019-12-23 09:30:29 +0700

    Re-order and add references

--- src/App.js
+++ src/App.js
@@ -31,23 +31,23 @@ const products = [
       'https://baselinenutritionals.net/collections/frontpage/products/phi-zymes%C2%AE-450-capsules',
     youtube: 'https://www.youtube.com/watch?v=dgJGmCmh9uI&feature=youtu.be',
     benefits: [
-      "World's most powerful systemic proteolytic enzyme formula to help facilitate injury recovery.",
-      'Helps your body clean out and repair the cardiovascular system for health or athletic performance.',
+      'Proteolytic enzymes have been used since early 1900s as natural protocol to heal cancer',
+      'Mode of action breaks down fibrin coatings of cancer cells',
+      'Potentially allows lower does of chemotherapy thereby reducing side-effects incurred',
       'Reduces Circulating Immune Complexes that can lead to allergies and systemic inflammation.',
       'Nutritionally enhances the quality of blood.',
       'Helps with factors that can cause autoimmune disorders.',
       'Helps clean out and repair the cardiovascular system',
       'Helps minimize the risk of blood clots',
       'Helps reduce scar tissue',
-      'Digest fibroid cysts',
-      'Proteolytic enzymes have been used since early 1900s as natural protocal to heal cancer',
-      'Mode of action breaks down fibrin coatings of cancer cells',
-      'Potentially allows lower does of chemotherapy thereby reducing side-effects incurred',
-      'Decreases inflammation'
+      'Digest fibroid cysts'
     ],
     references: [
       'https://thetruthaboutcancer.com/systemic-proteolytic-enzymes/',
-      'https://articles.mercola.com/sites/articles/archive/2018/03/05/proteolytic-enzymes-benefits.aspx'
+      'https://articles.mercola.com/sites/articles/archive/2018/03/05/proteolytic-enzymes-benefits.aspx',
+      'https://draxe.com/nutrition/proteolytic-enzymes/',
+      'https://www.jonbarron.org/article/proteolytic-enzyme-formula',
+      'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4188883/'
     ]
   }
 ];
@@ -59,7 +59,7 @@ function App() {
     <div className={classes.root}>
       <div className={classes.container}>
         <main className={classes.content}>
-          <h1>Alternative Medicine</h1>
+          <h1>Alternative Treatment</h1>
           <h2>Proposed Treatment</h2>
           <p>
             Morning on an empty stomach (at least 45 mins away from food), Take
